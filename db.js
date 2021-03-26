2e96cd0423c5294fc88f6adeca92499f9729b60f
Murilo Polese <murilopolese@gmail.com>
2021-03-26 20:05:03 +0100

    Generate RSS

--- db.js
+++ db.js
@@ -1,172 +1,212 @@
-window.db = [
+let db = [
   {
     title: "Carrés en deux positions 4",
     research: "./research/output/vera-molnar.html",
     display: "./p5js/vera-moln-rcarr-s-en-deux-positions-4",
+    date: '2020-07-01'
   },
   {
     title: "Machine imaginaire",
     research: "./research/output/vera-molnar.html",
     display: "./p5js/vera-moln-rimaginary-machine",
+    date: '2021-03-26'
   },
   {
     title: "Interruptions",
     research: "./research/output/vera-molnar.html",
     display: "./p5js/vera-moln-rinterruptions",
+    date: '2021-03-26'
   },
   {
     title: "Random digits",
     research: "./research/output/vera-molnar.html",
     display: "./p5js/random-digits-animation",
+    date: '2021-03-26'
   },
   {
     title: "Linear figure",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneylinear-figure-3",
+    date: '2021-03-26'
   },
   {
     title: "Linear figure",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneylinear-figure-1",
+    date: '2021-03-26'
   },
   {
     title: "Linear figure",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneylinear-figure-2",
+    date: '2021-03-26'
   },
   {
     title: "Linear figure",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneylinear-figure-4-two",
+    date: '2021-03-26'
   },
   {
     title: "Matrix study",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneymatrix-study-1",
+    date: '2021-03-26'
   },
   {
     title: "Permutations",
     research: "./research/output/john-withney.html",
     display: "./p5js/john-withneypermutations",
+    date: '2021-03-26'
   },
   {
     title: "Means and message",
     research: "./research/output/muriel-cooper.html",
     display: "./p5js/muriel-coopermeans-and-message",
+    date: '2021-03-26'
   },
   {
     title: "Means and message (responsive animation)",
     research: "./research/output/muriel-cooper.html",
     display: "./p5js/muriel-coopermeans-and-message-responsive",
+    date: '2021-03-26'
   },
   {
     title: "Draft notation",
     research: "./research/output/anni-albers.html",
     display: "./p5js/anni-albersdraft-notation",
+    date: '2021-03-26'
   },
   {
     title: "Weaving animation study",
     research: "./research/output/anni-albers.html",
     display: "./p5js/weaving-animation-study-1",
+    date: '2021-03-26'
   },
   {
     title: "Weaving animation study",
     research: "./research/output/anni-albers.html",
     display: "./p5js/weaving-animation-study-2",
+    date: '2021-03-26'
   },
   {
     title: "Wireworld",
     research: "./research/output/ken-knowlton.html",
     display: "./p5js/cellular-automata-wireworld-1",
+    date: '2021-03-26'
   },
   {
     title: "Wireworld: Rule 110",
     research: "./research/output/ken-knowlton.html",
     display: "./p5js/cellular-automatawireworld-rule-110",
+    date: '2021-03-26'
   },
   {
     title: "Satoshi Aizawa",
     research: "https://twitter.com/satoshi_aizawa",
     display: "./p5js/satoshi-aizawa",
+    date: '2021-03-26'
   },
   {
     title: "Fantasia exata",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeirofantasia-exata-1",
+    date: '2021-03-26'
   },
   {
     title: "Ideia visivel",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiroideia-visivel",
+    date: '2021-03-26'
   },
   {
     title: "Ideia visivel",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiroideia-visivel-2",
+    date: '2021-03-26'
   },
   {
     title: "Estrutura determinada e determinante",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiroestrutura-determinada-e-determinante",
+    date: '2021-03-26'
   },
   {
     title: "Meander",
     research: "./research/output/anni-albers.html",
     display: "./p5js/anni-albers-meander",
+    date: '2021-03-26'
   },
   {
     title: "Sem Titulo",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeirosem-titulo",
+    date: '2021-03-26'
   },
   {
     title: "Idéia Visível",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiroideia-visivel-3",
+    date: '2021-03-26'
   },
   {
     title: "Inércia",
     research: "https://hnarte.wordpress.com/2010/07/29/axidioma-de-cara-nova-e-em-idioma-poetico/",
     display: "./p5js/ivo-godoy-inercia",
+    date: '2021-03-26'
   },
   {
     title: "Estrutura determinada e determinante",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiroestrutura-determinada-e-determinante-2",
+    date: '2021-03-26'
   },
   {
     title: "Intersections",
     research: "./research/output/waldemar-cordeiro.html",
     display: "./p5js/waldemar-cordeiro-intersections",
+    date: '2021-03-26'
   },
   {
     title: "M",
     research: "./research/output/abraham-palatnik.html",
     display: "./p5js/abraham-palatnik-m1",
+    date: '2021-03-26'
   },
   {
     title: "Source code",
     research: "https://tecnoxamanismo.wordpress.com/",
     display: "./p5js/helicoidal-lines",
+    date: '2021-03-26'
   },
   {
     title: "M",
     research: "./research/output/abraham-palatnik.html",
     display: "./p5js/abraham-palatnik-m2",
+    date: '2021-03-26'
   },
   {
     title: "Costigliolo 2",
     research: "https://www.jcmac.art/artists/costigliolo-jose-pedro/",
     display: "./p5js/costigliolo-2",
+    date: '2021-03-26'
   },
   {
     title: "Costigliolo 4",
     research: "https://www.jcmac.art/artists/costigliolo-jose-pedro/",
     display: "./p5js/costigliolo-4",
+    date: '2021-03-26'
   },
   {
     title: "Computer images",
     research: "./research/output/vasulkas.html",
-    display: "./p5js/vasulkas-computer-images"
+    display: "./p5js/vasulkas-computer-images",
+    date: '2021-03-26'
   },
 ]
+
+try {
+  window.db = db
+} catch(e) {
+  module.exports = db
+}
